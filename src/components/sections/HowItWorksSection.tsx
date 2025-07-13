feddc51c7423716eeed12c46fe952040b722e3b2
Konstantin <158179026+cvccvc40@users.noreply.github.com>
2025-07-13 15:29:16 +0300

    Added base structure for FAQ section, also, added custom font to globals.css

--- src/components/sections/HowItWorksSection.tsx
+++ src/components/sections/HowItWorksSection.tsx
@@ -21,62 +21,85 @@ export const HowItWorksSection = () => {
   ];
 
   return (
-    <section
-      style={{
-        backgroundImage: "url('/how-it-works-bg.jpg')",
-      }}
-      className="relative bg-black rounded-t-4xl overflow-hidden bg-contain  bg-center py-[145px] bg-size-[150%_150%] md:bg-size-[100%_100%]"
-    >
-      {/* Content */}
-      <div className="section-container flex flex-col justify-center">
-        {/* Header Section */}
-        <div className="flex flex-col lg:flex-row justify-between items-center mb-16 lg:mb-36 gap-x-4 gap-y-6">
-          {/* Main Heading */}
-          <div className="max-w-[580px] w-full">
-            <h2 className="text-white font-semibold text-4xl lg:text-6xl leading-tight">
-              Build a Winning <span className="text-accent-500">Resume</span>
-              <br />
-              That <span className="text-accent-500">Stands Out</span>
-            </h2>
-          </div>
-
-          {/* Description */}
-          <div className="max-w-[580px] text-left lg:text-right">
-            <p className="text-white font-light text-xl lg:text-2xl leading-relaxed">
-              Craft your perfect resume with AI-enhanced suggestions,
-              customizable templates, and a process so simple, it feels like
-              magic.
-            </p>
-          </div>
-        </div>
-
-        {/* Steps Section */}
-        <div className="flex flex-col lg:flex-row justify-center gap-4">
-          {steps.map((step, index) => (
-            <div
-              key={index}
-              className="bg-zinc-700 rounded-3xl p-3 w-full min-h-40 flex items-center gap-6"
-            >
-              {/* Number Circle */}
-              <div className="w-18 h-18 rounded-full border-4 border-accent-500 flex items-center justify-center p-2 flex-shrink-0">
-                <span className="text-accent-500 font-medium text-3xl leading-tight">
-                  {step.number}
-                </span>
-              </div>
-
-              {/* Text Content */}
-              <div className="flex-1">
-                <h3 className="text-white font-medium text-2xl leading-tight mb-4">
-                  {step.title}
-                </h3>
-                <p className="text-white font-normal text-xl leading-relaxed">
-                  {step.description}
-                </p>
-              </div>
+    <div>
+      <section
+        style={{
+          backgroundImage: "url('/how-it-works-bg.jpg')",
+        }}
+        className="relative bg-black rounded-t-4xl overflow-hidden bg-contain  bg-center py-[145px] bg-size-[150%_150%] md:bg-size-[100%_100%]"
+      >
+        {/* Content */}
+        <div className="section-container flex flex-col justify-center">
+          {/* Header Section */}
+          <div className="flex flex-col lg:flex-row justify-between items-center mb-16 lg:mb-36 gap-x-4 gap-y-6">
+            {/* Main Heading */}
+            <div className="max-w-[580px] w-full">
+              <h2 className="text-white font-semibold text-4xl lg:text-6xl leading-tight">
+                Build a Winning <span className="text-accent-500">Resume</span>
+                <br />
+                That <span className="text-accent-500">Stands Out</span>
+              </h2>
             </div>
-          ))}
+
+            {/* Description */}
+            <div className="max-w-[580px] text-left lg:text-right">
+              <p className="text-white font-light text-xl lg:text-2xl leading-relaxed">
+                Craft your perfect resume with AI-enhanced suggestions,
+                customizable templates, and a process so simple, it feels like
+                magic.
+              </p>
+            </div>
+          </div>
+
+          {/* Steps Section */}
+          <div className="flex flex-col lg:flex-row justify-center gap-4">
+            {steps.map((step, index) => (
+              <div
+                key={index}
+                className="bg-zinc-700 rounded-3xl p-3 w-full min-h-40 flex items-center gap-6"
+              >
+                {/* Number Circle */}
+                <div className="w-18 h-18 rounded-full border-4 border-accent-500 flex items-center justify-center p-2 flex-shrink-0">
+                  <span className="text-accent-500 font-medium text-3xl leading-tight">
+                    {step.number}
+                  </span>
+                </div>
+
+                {/* Text Content */}
+                <div className="flex-1">
+                  <h3 className="text-white font-medium text-2xl leading-tight mb-4">
+                    {step.title}
+                  </h3>
+                  <p className="text-white font-normal text-xl leading-relaxed">
+                    {step.description}
+                  </p>
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+      <section
+      className="relative bg-white rounded-t-4xl"
+      >
+        <div className="flex flex-col justify-center gap-10">
+          <div>
+            <h5 className="font-noto-sans font-semibold p-2 text-center leading-12">Frequently asked questions</h5>
+          </div>
+          <div className="flex flex-col justify-center">
+            <div>
+              <div>
+                <p>Is Foliofy free to use?</p>
+                <div></div>
+              </div>
+              <p className="text-lg leading-6 text-zinc-700 w-[361px]">Foliofy offers a free plan with basic features and limited number of resume downloads. Our premium plans unlock advanced AI features, more templates, and unlimited downloads.</p>
+            </div>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+        </div>
+      </section>
+    </div>
   );
 };
